963ca7fb416bfe797bb651544258b1af454ceb2e
Oscar-725 <oscar.rosalipo@gmail.com>
2023-02-10 15:19:05 -0600

    Initial commit

---
+++ app/src/main/java/com/example/fechaactual/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.fechaactual
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fechaactual.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.time.ZoneOffset.UTC
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    val fecha:String = obtenerFechaConFormato()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        //manera simplificada
+       // binding.fecha.text = SimpleDateFormat("dd MMM. yyyy, hh:mm a").format(Date())
+
+
+        binding.fecha.text = fecha
+    }
+
+    fun obtenerFechaConFormato ( ) :String {
+        val calendar: Calendar = Calendar.getInstance()
+        val date: Date = calendar.time
+        var sdf: SimpleDateFormat
+        sdf = SimpleDateFormat("dd MMM. yyyy, hh:mm a")
+        return sdf.format(date)
+    }
+
+
+
+
+
+}
